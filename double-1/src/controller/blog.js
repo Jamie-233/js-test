67d6e46e23a9685b7f881d1d9559cacf47873bc9
jiancun <jiancun.zhao@70capital.com>
2020-08-29 20:57:25 +0800

    modify

--- double-1/src/controller/blog.js
+++ double-1/src/controller/blog.js
@@ -2,7 +2,7 @@ const getList = (author, keyword) => {
   return [
     {
       id: 1,
-      title: 'Linux设计与实现',
+      title: 'Linux 设计与实现',
       content: '很难',
       author: 'Jenkin',
       createTime: 12233344455
@@ -10,7 +10,7 @@ const getList = (author, keyword) => {
     {
       id: 2,
       title: 'Node.js',
-      content: '很难...',
+      content: '...',
       author: 'Jenkin',
       createTime: 12233344455
     }
