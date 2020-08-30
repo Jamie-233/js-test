d0ac9d643ac7b8643b859ee96f2655d4429d3861
jiancun <jiancun.zhao@70capital.com>
2020-08-30 18:55:22 +0800

    bug fixes

--- double-1/src/controller/blog.js
+++ double-1/src/controller/blog.js
@@ -7,7 +7,7 @@ const getList = (author, keyword) => {
     sql += `and author='${author}' `
   }
   if(keyword) {
-    sql += `and title like %${keyword}% `
+    sql += `and title like '%${keyword}%' `
   }
   sql += `order by createtime desc;`
 
